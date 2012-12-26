25e91cf6f427f1c842a8eb15d46752917b2411ab
miehilmie <mhilmi1117@gmail.com>
2012-12-26 15:09:02 +0800

    more ajax controller

---
+++ application/controllers/ajax/universities.php
@@ -0,0 +1,52 @@
+
+<?php
+
+class Ajax_Universities_Controller extends Base_Controller {
+
+	public $restful = true;    
+
+	public function get_index()
+    {
+
+    }    
+    
+    public function get_faculties_index($id) {
+		if($id == 'none') {
+			return json_encode(array('err' => true));
+		}
+		
+		$f = University::find($id)->faculties()->get();
+		return eloquent_to_json($f);
+    }
+
+	public function post_index()
+    {
+
+    }    
+
+	public function get_show()
+    {
+
+    }    
+
+	public function get_edit()
+    {
+
+    }    
+
+	public function get_new()
+    {
+
+    }    
+
+	public function put_update()
+    {
+
+    }    
+
+	public function delete_destroy()
+    {
+
+    }
+
+}
