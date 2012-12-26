b7f3f689ec8dd8e779544f98cb22f585dfea06aa
miehilmie <mhilmi1117@gmail.com>
2012-12-26 15:06:17 +0800

    show user html

--- application/views/user/show.blade.php
+++ application/views/user/show.blade.php
@@ -1,1 +1,34 @@
-This is the F:\Apache24\vhost\groupus.dev\application\views/user/show.blade.php view
+@layout('logged')
+
+@section('styles')
+@parent
+{{ HTML::style('css/student.css') }} 
+@endsection
+
+@section('javascripts')
+@parent
+{{ HTML::script('js/student.js') }}
+@endsection
+
+@section('content')
+<table border="1" align="center">
+	<tr>
+		<td colspan="2"><h1>{{ $name }}</h1></td>
+	</tr>
+	<tr>
+		<td>{{ HTML::image($img_url) }}</td>
+		<td>
+			<div>Name : {{ $name }}</div>
+			<div>Age : {{ $age }}</div>
+			<div>Address: {{ $address }}</div>
+			<div>Phone: {{ $phone }}</div>
+			<div>Faculty: {{ $faculty->name }} ({{ $faculty->abbrevation }})</div>
+			<div>University: {{ $university }}</div>
+		</td>
+	</tr>
+	<tr><td colspan="2">Friendly: * * * *</td><tr>
+	<tr><td colspan="2">Overall proficiency: * * * *</td><tr>
+	<tr><td colspan="2">Hardworking: * * * *</td><tr>
+	<tr><td colspan="2">Leadership: * * * *</td><tr>
+</table>
+@endsection
