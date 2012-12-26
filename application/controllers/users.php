b66ffbf8133867eec9e948a95ca7d1f7faed8fdd
miehilmie <mhilmi1117@gmail.com>
2012-12-26 15:05:55 +0800

    add user logic for show_user or users/(:id)

--- application/controllers/users.php
+++ application/controllers/users.php
@@ -11,12 +11,29 @@ class Users_Controller extends Base_Controller {
 
 	public function post_index()
     {
-
+        
     }    
 
 	public function get_show($id)
     {
-        return 'Specific user with id = '. $id . ' view';
+
+        if(!($u = User::find($id))) {
+            return View::make('error.404');
+        }
+        // @todo : add user column in db for
+        //          img_url, age, address, phone, 
+        $fac = $u->faculty()->first();
+        $uni = $u->university()->only('name');
+        return View::make('user.show')
+            ->with(array(
+                'img_url' => '',
+                'address' => 'Unit 8-9-3, Blok 8, Fasa 2, Pantai Hillpark, Jalan Pantai Dalam, 52900 Kuala Lumpur',
+                'age' => '23',
+                'name' => $u->name,
+                'phone' => '0177526474',
+                'faculty' => $fac,
+                'university' => $uni
+            ));
     }    
 
 	public function get_edit()
