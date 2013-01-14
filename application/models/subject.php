febfd094c6b6f4adea15a8532338275631c2746c
miehilmie <mhilmi1117@gmail.com>
2013-01-14 17:08:27 +0800

    avoid using static function for performance (using cache) change get_only function for better readibility

--- application/models/subject.php
+++ application/models/subject.php
@@ -37,99 +37,115 @@ class Subject extends Basemodel
 		->get();
 	}
 
-	public function get_only_discussions() {
+	public function subject_discussions() {
 		return $this->discussions()
 		->where('semester_id','=', Auth::user()->university->semester_id)
 		->order_by('created_at', 'desc')->get();
 	}
-	public function get_only_announcements() {
+	public function subject_announcements() {
 		return $this->announcements()
 		->where('semester_id','=', Auth::user()->university->semester_id)
 		->order_by('created_at', 'desc')->get();
 	}
 
-	public function get_only_students_and_lecturers() {
-		return $this->users()
-		->where('semester_id', '=', Auth::user()->university->semester_id)
-		->order_by('usertype_id', 'desc')->get();
-	}
 
-	public function get_only_onlinestudents() {
+	public function subject_onlinestudents() {
+		if(is_null($u = Auth::user())){
+			return false;
+		}
+
 		$five_minago = date('Y-m-d H:i:s',(time()- 5*60));
 
 		return $this->users()
-		->where('semester_id', '=', Auth::user()->university->semester_id)
+		->where('semester_id', '=', $u->university->semester_id)
 		->where('last_activity','>', $five_minago)
 		->where('usertype_id', '=', 1)
 		->order_by('name', 'asc')->get();
 	}
-	public function get_only_onlineusers() {
+	public function subject_onlineusers() {
+		if(is_null($u = Auth::user())){
+			return false;
+		}
 		$five_minago = date('Y-m-d H:i:s',(time()- 5*60));
 
 		return $this->users()
-		->where('semester_id', '=', Auth::user()->university->semester_id)
+		->where('semester_id', '=', $u->university->semester_id)
 		->where('last_activity','>', $five_minago)
 		->order_by('usertype_id', 'desc')
 		->order_by('name', 'asc')->get();
 	}
-	public function get_only_offlinestudents() {
+	public function subject_offlinestudents() {
+		if(is_null($u = Auth::user())){
+			return false;
+		}
 		$five_minago = date('Y-m-d H:i:s',(time()- 5*60));
 
 		return $this->users()
-		->where('semester_id', '=', Auth::user()->university->semester_id)
+		->where('semester_id', '=', $u->university->semester_id)
 		->where('last_activity','<=', $five_minago)
 		->where('usertype_id', '=', 1)
 		->order_by('name', 'asc')->get();
 	}
-	public function get_only_offlineusers() {
+	public function subject_offlineusers() {
+		if(is_null($u = Auth::user())){
+			return false;
+		}
 		$five_minago = date('Y-m-d H:i:s',(time()- 5*60));
 
 		return $this->users()
-		->where('semester_id', '=', Auth::user()->university->semester_id)
+		->where('semester_id', '=', $u->university->semester_id)
 		->where('last_activity','<=', $five_minago)
 		->order_by('usertype_id', 'desc')
 		->order_by('name', 'asc')->get();
 	}
-	public function get_only_grouprule() {
+	public function subject_grouprule() {
+		if(is_null($u = Auth::user())){
+			return false;
+		}
+
 		return $this->grouprules()
-		->where_semester_id(Auth::user()->university->semester_id)
+		->where_semester_id($u->university->semester_id)
 		->first();
 	}
 
-	public function get_only_groups() {
+	public function subject_groups() {
+		if(is_null($u = Auth::user())){
+			return false;
+		}
 
 		$groups = $this->groups()
-		->where_semester_id(Auth::user()->university->semester_id)
+		->where_semester_id($u->university->semester_id)
 		->get();
 
 		return $groups;
-		// $response = array();
-		// foreach ($groups as $g) {
-		// 	$o = json_decode(eloquent_to_json($g));
-		// 	$o->users = $g->students();
-
-		// 	$response[] = $o;
-		// }
-
-		// return json_encode($response);
 	}
-	// BOOLEAN
 
-	public static function IsEnrolled($id) {
-		return Auth::user()->subjects()
-		->where('subject_id','=',$id)
-		->where('semester_id', '=', Auth::user()->university->semester_id)
+	public function IsEnrolled() {
+		if(is_null($u = Auth::user())){
+			return false;
+		}
+
+		return $this->users()
+		->where('user_id','=', $u->id)
+		->where('semester_id', '=', $u->university->semester_id)
 		->count() > 0;
 	}
 
-	public static function IsFacultySubject($id) {
-		return static::where_faculty_id_and_id(Auth::user()->faculty_id, $id)
-		->count() > 0;
+	public function IsFacultySubject() {
+		if(is_null($u = Auth::user())){
+			return false;
+		}
+
+		return $this->faculty_id == $u->faculty_id;
 	}
 
 	public function IsGroupingEnable() {
+		if(is_null($u = Auth::user())){
+			return false;
+		}
+
 		return ($this->grouprules()
-		->where_semester_id(Auth::user()->university->semester_id)
-		->count() > 0) && $this->get_only_grouprule()->enable == true;
+		->where_semester_id($u->university->semester_id)
+		->count() > 0) && $this->subject_grouprule()->enable == true;
 	}
 }
